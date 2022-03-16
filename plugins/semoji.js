b4185ca3f2f9b8a1cae0783d8299e592cf8e9120
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-16 16:45:55 +0100

    Update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isPrems }) => {
 
 Can only 1 emoji, pay attention to spaces, don't spam!
 
-If you want to make a sticker, you can get 2/3 emoji? click http://wa.me/919744196187?text=%23stikeremoji`
+If you want to make a sticker, you can get 2/3 emoji? click http://wa.me/2347042930688?text=%23stikeremoji`
   if (!args[0]) throw er
 
   let template = (args[0] || '').toLowerCase()
