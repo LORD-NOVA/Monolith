4dbe8c71fdb9e1567b21e84238d6a92d44bed9f3
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-16 12:49:59 +0100

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,6 +1,6 @@
 const { facebook } = require('../lib/facebook')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (!args[0]) throw `uhm.. where is the link dummy?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
   if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `url salah`
   facebook(args[0]).then(async res => {
     let fb = JSON.stringify(res)
