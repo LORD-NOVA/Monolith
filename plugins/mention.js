2954bb96f7abcf923616696a7bf414afddd4bf57
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-16 18:07:25 +0100

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Example:\n${usedPrefix + command} @919744196187`
+  if (!text) throw `Example:\n${usedPrefix + command} @2347042930688`
   m.reply(text, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
