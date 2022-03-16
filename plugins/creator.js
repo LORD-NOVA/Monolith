80a61a840f30e45be044eda08efd123f104b0c7e
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-16 12:33:02 +0100

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '2347042930688@s.whatsapp.net'), m)
+  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
