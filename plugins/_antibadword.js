e81c3a39f11474802d35063d5553c07fa1314d90
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-18 17:08:00 +0100

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -18,7 +18,7 @@ If the warning reaches 5 you will be banned
 type *#on antibadword* to turn on antibadword
 type *#sorry* to reduce the warning
 
-"Never use a big word when a little filthy one will do" (Wizard-Ser).`, watermark, 'Turn on Antibadword', ',1 antibadword',  'sorry', m)
+"Never use a big word when a little filthy one will do" (Monolith).`, watermark, 'Turn on Antibadword', ',1 antibadword',  'sorry', m)
         if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
