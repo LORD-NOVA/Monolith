57a2399cbe7878168e4dd424f1288a8250e1820d
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-26 22:03:14 +0100

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -10,26 +10,26 @@ handler.all = async function (m) {
     let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
-Hai, ${ucapan()}
-${user.banned ? 'you are banned' : 'Iam Wizard-Ser, please dont spam/call/ask to save to this number. Can I help you?, Type .menu To Get Bot Menu}
+Hi, ${ucapan()}
+${user.banned ? 'you are banned' : 'I am monolith, please dont spam/call/ask to save to this number. Can I help you?, Type .menu To Get Bot Menu}
 `.trim(), watermark, user.banned ? 'Bot Owner' : 'Menu', user.banned ? '.owner' : '.?', m)
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
-    const time = moment.tz('Asia/Kolkata').format('HH')
+    const time = moment.tz('Africa/Lagos').format('HH')
     res = "Good morning"
-    if (time >= 4) {
+    if (time >= 5) {
         res = "Good morning"
     }
-    if (time > 10) {
-        res = "Good afternoon"
-    }
-    if (time >= 15) {
+    if (time > 12) {
         res = "Good afternoon"
     }
     if (time >= 18) {
+        res = "Good afternoon"
+    }
+    if (time >= 20) {
         res = "Good night"
     }
     return res
