8b8b0ca0c9c2f4b3aeb9964afc8a743f9da9f4de
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-29 16:32:49 +0100

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'DEVILSER/Ammu'
+let repository = 'N0-VA/Wizard-ser'
 let branch = 'master'
 
 async function handler(m, { text }) {
