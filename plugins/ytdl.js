0d1e440979bece3cb9fbb5d364034dfff6ea7635
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-30 19:52:07 +0100

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -13,7 +13,7 @@ let handler = async (m, { itsu, args, isPrems, isOwner, usedPrefix, text }) => {
 await itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
                     "listMessage":  {
                         "title": `*Title*: ${title}`,
-                        "description": " \n_©ᴡɪᴢᴀʀᴅ sᴇʀ_ ",
+                        "description": " \n_©ᴍᴏɴᴏʟɪᴛʜ_ ",
                         "buttonText": `Quality`,
                         "listType": "SINGLE_SELECT",
                         "sections": [
