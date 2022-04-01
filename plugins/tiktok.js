37989a4fce4c0f7efa896d1ede07f740e0229980
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-01 21:57:08 +0100

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://vm.tiktok.com/`
+  if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://vt.tiktok.com/`
   if (!args[0].match(/tiktok/gi)) throw `wrong url`
 
   let res = await fetch(API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
