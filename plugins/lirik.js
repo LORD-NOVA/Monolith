6b8be948539e1596ef4ae8e0ae85e0dabe7dfe09
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 00:00:32 +0100

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} shape of you`
+  if (!text) throw `uhm.. what are you looking for?\n\nExample:\n${usedPrefix + command} Before you go`
   let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
