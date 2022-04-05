4b6da13f8779a98231575ae8781f32ef0304fce5
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 10:12:03 +0100

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -1,13 +1,15 @@
+// xie hua piao piao batman membuka kulkas dan mngambil semangka lalu memberikan mayo di atas semangka
+
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
+let handler = async(m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
-  if (!res.ok) throw eror
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', watermark, m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
 }
 handler.help = ['neko']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^neko$/i
 
 module.exports = handler
