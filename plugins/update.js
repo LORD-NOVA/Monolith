8f29b1c55b55fe70eecb5b76cfaa3b870498aaaa
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 13:41:18 +0100

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,16 +1,25 @@
 let { execSync } = require('child_process')
-
-let handler = async (m, { conn, text, isROwner }) => {
+let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/N0-VA/Wizard-ser.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
-    m.reply(stdout.toString())
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+    conn.reply(m.chat, stdout.toString(), m)
   }
 }
 handler.help = ['update']
 handler.tags = ['host']
 handler.command = /^update$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-handler.owner = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
 
 module.exports = handler
+
