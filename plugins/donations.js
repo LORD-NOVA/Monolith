81c365dba082f7313ff5a609bccec006a596fd86
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 13:24:14 +0100

    Create donations.js

---
+++ plugins/donations.js
@@ -0,0 +1,9 @@
+function handler(m) {
+  this.reply(m.chat, '🎀💮ᴅᴏɴᴀᴛɪᴏɴꜱ ᴀʀᴇ ɴᴇᴇᴅᴇᴅ ᴛᴏ ᴜᴘɢʀᴀᴅᴇ ʙᴏᴛ ꜱᴇʀᴠᴇʀꜱ ᴀɴᴅ ᴛᴏ ᴇɴᴄᴏᴜʀᴀɢᴇ ᴄᴏᴍᴍᴜɴɪᴛʏ\n💮ꜰᴏʀ ᴅᴏɴᴀᴛɪᴏɴꜱ ᴍᴇꜱꜱᴀɢᴇ ᴏᴡɴᴇʀ : wa.me/2347042930688🎀💮', m)
+}
+handler.help = ['Donations']
+handler.tags = ['tools']
+
+handler.command = /^(Donations|donations|donation)$/i
+
+module.exports = handler
