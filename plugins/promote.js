67b438c329cf91aadf307e70a3b10737e0780662
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 13:30:43 +0100

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -1,16 +1,21 @@
-let handler = async (m, { conn, participants }) => {
-  let members = participants.filter(member => !member.isAdmin).map(member => member.jid)
-  let users = m.mentionedJid.filter(user => members.includes(user))
-  for (let user of users) await conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
-}
-handler.help = ['promote','admin','^', '↑'].map(v => v + ' @user')
-handler.tags = ['admin']
-
-handler.command = /^(promote|admin|\^|↑)$/i
-
-handler.group = true
-
-handler.admin = true
-handler.botAdmin = true
-
-module.exports = handler
+let handler = async (m, { conn }) => {
+    let users = m.mentionedJid
+    conn.groupMakeAdmin(m.chat, users)
+  }
+  handler.help = ['promote','admin','^', '↑'].map(v => v + ' @user')
+  handler.tags = ['admins']
+  handler.command = /^(promote|admin|\^|↑)$/i
+  handler.owner = false
+  handler.mods = false
+  handler.premium = false
+  handler.group = true
+  handler.private = false
+  
+  handler.admin = true
+  handler.botAdmin = true
+  
+  handler.fail = null
+  
+  module.exports = handler
+  
+  
