66480087bdbf682e6cce9100b1c012e7896b2f30
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-05 10:23:34 +0100

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,15 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. where is the text?\n\nexample:\n${usedPrefix + command} hai`
-  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
+let handler = async (m, { text }) => {
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "en" }, ''))
   let json = await res.json()
-  if (json.result == 'I dont understand what you are saying. Please teach me.') await m.reply('The sim hasnt been taught yet, so I dont know t_t the custom message is on , Tell My Master To Teach Me..')
-  await m.reply(`*Simi:* ${json.result}`)
+  if (json.success) m.reply(json.success)
+  else throw json
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.help = ['bot'].map(v => v + ' <teks>')
 handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
+handler.command = /^(bot|simi)$/i
 
 module.exports = handler
 
