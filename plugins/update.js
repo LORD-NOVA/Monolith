c6705cf811564f8d16aacb0432cb1fa4ff962888
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-06 16:27:12 +0100

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,25 +1,16 @@
 let { execSync } = require('child_process')
-let handler = async (m, { conn, text }) => {
+
+let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, stdout.toString(), m)
+    let stdout = execSync('git remote set-url origin https://github.com/N0-VA/Monolith.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
   }
 }
 handler.help = ['update']
-handler.tags = ['host']
+handler.tags = ['owner']
 handler.command = /^update$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.owner = true
 
 module.exports = handler
-
