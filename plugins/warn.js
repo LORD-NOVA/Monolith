b439be7eeecb6dd475e820e1a586812cce6b135b
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-06 09:41:25 +0100

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -1,79 +1,34 @@
-let handler = async (m, { conn, text }) => {
-	function no(number){
-    return number.replace(/\s/g,'').replace(/([@+-])/g,'')
-  }
-
-  text = no(text)
-	
-  if(isNaN(text)) {
-		var number = text.split`@`[1]
-  } else if(!isNaN(text)) {
-		var number = text
-  }
-
-  if(!text && !m.quoted) return conn.reply(m.chat, `*Tag the user, write the number, or reply to the member you want to _warning_*`, m)
-  //let exists = await conn.isOnWhatsApp(number)
-  // if (exists) return conn.reply(m.chat, `*Nomor target tidak terdaftar di WhatsApp*`, m)
-  if(isNaN(number)) return conn.reply(m.chat, `*The number you entered is invalid !*`, m)
-  if(number.length > 15) return conn.reply(m.chat, `*The number you entered is invalid!*`, m)
-  try {
-		if(text) {
-			var user = number + '@s.whatsapp.net'
-		} else if(m.quoted.sender) {
-			var user = m.quoted.sender
-		} else if(m.mentionedJid) {
-  		  var user = number + '@s.whatsapp.net'
-			}  
-		} catch (e) {
-  } finally {
-
-	
-  if(typeof global.DATABASE.data.users[user] == 'undefined') {
-  global.DATABASE._data.users[user] = {
-			exp: 0,
-			limit: 10,
-			lastclaim: 0,
-			warning: 0,
-			command: 0,
-			job: "x",
-			price: 0,
-			chat: 0,
-			whitelist: false,
-			isBanned: false,
-			spam: 0
-		}
-	}
-
-	let groupMetadata = m.isGroup ? await conn.groupMetadata(m.chat) : {}
-  let participants = m.isGroup ? groupMetadata.participants : []
-	let users = m.isGroup ? participants.find(u => u.jid == user) : {}
-	let isAdmin = users.isAdmin || users.isSuperAdmin || false
-	let number = user.split('@')[0]
-
-	if(isAdmin) return conn.reply(m.chat, `*Can't give a warning to fellow admins !*`, m)
-  
-	
-	global.DATABASE.data.users[user].warning += 1
-	var warn = global.DATABASE.data.users[user].warning
- 	if(warn > 4) {
- 		conn.reply(m.chat, `*❏ WARNING*\n\nYou've been warned 5 times! Just kick it!`, null, {contextInfo: {
-          mentionedJid: [user]
- 	}}).then(() => {
- 		conn.groupRemove(m.chat, [user])
- 	})
- } else {
- 	conn.reply(m.chat, `*❏ WARNING*\n\n@${number} : [ ${warn} / 5 ]\n\nIf you get a warning up to 5 times, you will be automatically kicked !\n\nType *.delwarn* to delete the warning by paying the limit`, null, {contextInfo: {
-          mentionedJid: [user]
- 	}})
+let handler = async (m, { conn, args, groupMetadata}) => {
+    if (args.length > 0) {
+  const time = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
- 
- }
+        let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : false
+        let warn = global.db.data.users[mention].warn
+        if (warn < 2) {
+            global.db.data.users[mention].warn += 1
+            conn.reply(m.chat, `⚠️ *WARNING +1*`, m)
+            m.reply('Kamu mendapatkan warn dari admin, total warn kamu sekarang *' + (warn + 1) + '* warn, Jika kamu mendapat warn *3 kali*, kamu akan dikeluarkan dari grup', mention)
+        } else if (warn == 2) {
+            global.db.data.users[mention].warn = 0
+            m.reply('Selamat tinggal')
+                await time(5000)
+             await conn.groupRemove(m.chat, [mention])
+             m.reply(`Kamu dikeluarkan dari group ${groupMetadata.subject} karena telah mendapat 3 kali warn`, mention)
+           
+        }
+    } else conn.reply(m.chat, 'Tag target', m)
 }
-handler.help = ['warn *(reply)*','warn *@tag*']
-handler.tags = ['admin']
+handler.help = ['Warn @user']
+handler.tags = ['group']
 handler.command = /^warn$/i
-handler.admin = true
+handler.owner = false
+handler.mods = false
+handler.premium = false
 handler.group = true
+handler.private = false
+handler.register = false
+handler.admin = true
 handler.botAdmin = true
+
 module.exports = handler
