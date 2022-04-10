d0463d9e5148da991d4686debb2e447a92d06c5d
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-10 14:40:19 +0100

    Create bucin4.js

---
+++ plugins/bucin4.js
@@ -0,0 +1,30 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+  await conn.sendButton(m.chat, `“${pickRandom(global.bucin)}”`, watermark, 'Good Night', `${usedPrefix + command}`)
+}
+handler.help = ['goodnight']
+handler.tags = ['quotes']
+handler.command = /^(goodnight|gn)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.bucin = [
+  "😘𝙂𝙤𝙤𝙙 🙈𝙣𝙞𝙜𝙝𝙩 💫✨",
+  "🤗𝓖𝓸𝓸𝓭 🧚‍♀𝓷𝓲𝓰𝓱𝓽 ❄️✨",
+  "🌌❡០០ᖱ 🌙⩎ɨ❡ϦƬ 🌎",
+  "😘ցօօժ ⭐️ղíցհԵ 💝",
+  "🌃Ꮐᝪᝪᗞ 🙈ᑎᏆᏀᕼᎢ 💫✨",
+]
