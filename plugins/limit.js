276bfea55b2c88da8b9b6c2d17949ad49a156a26
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-12 16:02:05 +0100

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,9 +3,9 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`*${user.limit}* Limit\n*${user.exp}* XP\nLevel *${user.level}*\nRole *${user.role}*`)
+    m.reply(`✨*${user.limit}* Limit\n\n🏮*${user.exp}* XP\n\n🎐Level *${user.level}*\n\n🧧Role *${user.role}*`)
 }
-handler.help = ['my [@user]']
+handler.help = ['my [@user]' , 'level [@user]']
 handler.tags = ['xp']
-handler.command = /^(my|limit)$/i
+handler.command = /^(my|limit|level)$/i
 module.exports = handler
