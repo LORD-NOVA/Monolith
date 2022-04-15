259591cb35ee90b9ca14e71767f3f04b90217fa4
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-15 13:33:27 +0100

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
+let handler = async (m, { msg }) => {
   let res = await fetch(global.API('https://api.brainshop.ai', '/get?bid=164410&key=ctTB3IBU2TLXGflz&uid=[uid]&msg=', { msg: encodeURIComponent(msg) }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
 }
-handler.help = ['bot'].map(v => v + ' <teks>')
+handler.help = ['bot'].map(v => v + ' <text>')
 handler.tags = ['fun']
 handler.command = /^(bot|simi)$/i
 
