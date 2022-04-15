63d574e98136a2dd2bde1234b7d2a411f433da68
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-15 12:52:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ global.APIs = { // API Prefix
   zahir: 'https://zahirr-web.herokuapp.com',
   zekais: 'http://zekais-api.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
+  brainshop: 'http://api.brainshop.ai',
 }
 global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
@@ -28,6 +29,7 @@ global.APIKeys = { // APIKey nya disini
   'https://api.lolhuman.xyz': 'sdz8VotwEnDd4HvdIUfy1e4qTDx',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
+  'https://api.brainshop.ai': 'ctTB3lBU2TLXGflz',
 }
 
 // Sticker WM
