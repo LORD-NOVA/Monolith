57ee864f4224b5fd1f28d98574ea08c6826f25c9
Nova <98613828+N0-VA@users.noreply.github.com>
2022-04-15 09:10:07 +0100

    Update donations.js

--- plugins/donations.js
+++ plugins/donations.js
@@ -1,9 +1,9 @@
 function handler(m) {
-  this.reply(m.chat, '🎀💮ᴅᴏɴᴀᴛɪᴏɴꜱ ᴀʀᴇ ɴᴇᴇᴅᴇᴅ ᴛᴏ ᴜᴘɢʀᴀᴅᴇ ʙᴏᴛ ꜱᴇʀᴠᴇʀꜱ ᴀɴᴅ ᴛᴏ ᴇɴᴄᴏᴜʀᴀɢᴇ ᴄᴏᴍᴍᴜɴɪᴛʏ\n💮ꜰᴏʀ ᴅᴏɴᴀᴛɪᴏɴꜱ ᴍᴇꜱꜱᴀɢᴇ ᴏᴡɴᴇʀ : wa.me/2347042930688🎀💮', m)
+  this.reply(m.chat, '🎀💮ᴅᴏɴᴀᴛɪᴏɴꜱ ᴀʀᴇ ɴᴇᴇᴅᴇᴅ ᴛᴏ ᴜᴘɢʀᴀᴅᴇ ʙᴏᴛ ꜱᴇʀᴠᴇʀꜱ ᴀɴᴅ ᴛᴏ ᴇɴᴄᴏᴜʀᴀɢᴇ ᴄᴏᴍᴍᴜɴɪᴛʏ\n\n💮ꜰᴏʀ ᴅᴏɴᴀᴛɪᴏɴꜱ ᴍᴇꜱꜱᴀɢᴇ ᴏᴡɴᴇʀ:\nwa.me/2347042930688 🎀💮', m)
 }
 handler.help = ['Donations']
 handler.tags = ['tools']
 
-handler.command = /^(Donations|donations|donation)$/i
+handler.command = /^(Donations|donate|Donate|donation)$/i
 
 module.exports = handler
